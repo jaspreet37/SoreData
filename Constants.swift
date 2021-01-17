21e606b580e1ca36ecf2aebeb9fdbd2dd21883c9
jaspreet37 <77575894+jaspreet37@users.noreply.github.com>
2021-01-17 19:28:33 +0530

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  LaundryDay_Driver
+//
+//  Created by cql on 26/05/20.
+//  Copyright © 2020 dev. All rights reserved.
+//
+
+import UIKit
+
+enum DefaultKeys: String
+{
+    case socialId
+    case switchState
+    case authKey
+    case deviceToken
+    case userDetails
+    case latitude
+    case longitude
+    case location
+}
